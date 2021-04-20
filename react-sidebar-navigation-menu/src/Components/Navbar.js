f2543a9de782ad3ba2761a7c5e55970ef419e5d7
Serhii <malgothsm@gmail.com>
2021-04-20 18:43:47 +0300

    Adding onclick func and useState for Navbar component

--- react-sidebar-navigation-menu/src/Components/Navbar.js
+++ react-sidebar-navigation-menu/src/Components/Navbar.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as FaIcons from "react-icons/fa";
+import * as VscIcons from "react-icons/vsc";
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 
+
+
 function Navbar() {
+  const [sidebar, setSidebar ] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);  
   return (
     <>
       <div className="navbar">
         <Link className="menu-bars" to="/" >
-          <FaIcons.FaBars />
+          <FaIcons.FaBars onClick={showSidebar} />
         </Link>
       </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <VscIcons.VscChromeClose />
+            </Link>
+          </li>
+        </ul> 
+      </nav>
     </>
   )
 }
