7edc4e521b0e604e9ba37985855a8d7b8f630214
Serhii <malgothsm@gmail.com>
2021-04-29 17:28:25 +0300

    Working on menu

--- react-dropdown-navbar-menu/src/Components/Navbar.js
+++ react-dropdown-navbar-menu/src/Components/Navbar.js
@@ -5,12 +5,20 @@ import Dropdown from './Dropdown';
 import './Navbar.css';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <>
       <nav className="navbar">
         <Link to='/' className="navbar-logo">
           Logo
         </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          
+        </ul>
       </nav>
     </>
   )
