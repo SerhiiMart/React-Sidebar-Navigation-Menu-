02c438b5751ef98b736a2ae3850e4a237acba83b
Serhii <malgothsm@gmail.com>
2021-04-30 16:02:10 +0300

    Added menu-items for the menu

---
+++ react-dropdown-navbar-menu/src/Components/Dropdown.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {menuItems} from './Menu-Items';
+import {Link} from 'react-router-dom';
+import './Dropdown.css';
+
+function Dropdown() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Dropdown;
---
+++ react-dropdown-navbar-menu/src/Components/Menu-Items.js
@@ -0,0 +1,24 @@
+////Variables for the dropdown menu
+export const menuItems = [
+  {
+    title: "Marketing",
+    path: "/marketing",
+    className: "dropdown-link"
+  },
+  {
+    title: "Consulting",
+    path: "/consulting",
+    className: "dropdown-link"
+  },
+  {
+    title: "Design",
+    path: "/design",
+    className: "dropdown-link"
+  },
+  {
+    title: "Development",
+    path: "/development",
+    className: "dropdown-link"
+  }
+];
+
