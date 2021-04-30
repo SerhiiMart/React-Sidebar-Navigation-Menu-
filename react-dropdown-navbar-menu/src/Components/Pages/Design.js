6db3ef8c210b4c1b3e98694593c3b17399cf043f
Serhii <malgothsm@gmail.com>
2021-04-30 19:40:57 +0300

    Added pages

---
+++ react-dropdown-navbar-menu/src/Components/Pages/Design.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Design() {
+  return (
+    <>
+      <h1 className="design">Design</h1>
+    </>
+  )
+}
+
---
+++ react-dropdown-navbar-menu/src/Components/Pages/Development.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Development() {
+  return (
+    <>
+      <h1 className="development">Development</h1>
+    </>
+  )
+}
+
