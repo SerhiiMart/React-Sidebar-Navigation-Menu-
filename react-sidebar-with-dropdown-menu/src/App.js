7f5262bf114f57dc8a373ba36c8d0e45fec5f299
Serhii <malgothsm@gmail.com>
2021-06-18 18:49:55 +0300

    Need to fix some bugs

--- react-sidebar-with-dropdown-menu/src/App.js
+++ react-sidebar-with-dropdown-menu/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Overview from './pages/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
+import Team from './pages/Team';
 
 function App() {
   return (
