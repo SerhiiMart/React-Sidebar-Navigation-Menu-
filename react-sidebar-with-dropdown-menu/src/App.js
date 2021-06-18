77092b8abd1e3e1a3414244da734464ccfc805df
Serhii <malgothsm@gmail.com>
2021-06-18 18:45:06 +0300

    Added pages and components

--- react-sidebar-with-dropdown-menu/src/App.js
+++ react-sidebar-with-dropdown-menu/src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path='/overview' exact component={Overview} />
+        <Route path='/reports' exact component={Reports} />
+        <Route path='/reports/reports1' exact component={ReportsOne} />
+        <Route path='/reports/reports2' exact component={ReportsTwo} />
+        <Route path='/reports/reports3' exact component={ReportsThree} />
+        <Route path='/team' exact component={Team} />
+      </Switch>
+    </Router>
   );
 }
 
